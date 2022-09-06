cadba06ebea82499bcaa4f4803a9151c2a012365
treysyroka <treysyroka23@yahoo.com>
2022-09-06 14:25:34 -0400

    Time homework

--- src/HW2/DayOfWeek.java
+++ src/HW2/DayOfWeek.java
@@ -1,4 +1,19 @@
 package HW2;
 
+import java.util.Scanner;
+
 public class DayOfWeek {
+
+    public static int Date(int d, int m, int y) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print
+    }
 }
+
+
+
--- src/Hw1/Time.java
+++ src/Hw1/Time.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 
 public class Time {
     public static int midnight(int h, int m, int s)
+            // This is the formula i used to change everything into seconds
     { return ((h * 60 * 60) + (m * 60) + s);
 
     }
-
-
-
-
-
-
+    
     public static void main(String [] args) {
         Scanner kbd = new Scanner(System.in);
-
+// saving variables that the users can enter in their answers
+        // this will also print out the questions I am asking
         System.out.print("Enter the hour of the day: ");
         int hour = kbd.nextInt();
 
@@ -27,7 +24,8 @@ public class Time {
         double line = (midnight(hour, minute, second));
         int remainder = (24 * 60 * 60) - (midnight(hour, minute, second));
         double day = 86400;
-
+// using doubles for some of my variables above allows me to use the math.round while also being able to
+        // successfully print out the correct answer
         double x = Math.round((line / day) * 100);
 
         // 86400 is the ammount of seconds in a full day
