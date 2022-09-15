3df3eca7e84bb608ee7e9fd3f0a460f44850780f
treysyroka <treysyroka23@yahoo.com>
2022-09-14 23:34:25 -0400

    Compounding interest

--- src/InvestmentCalculator.java
+++ src/InvestmentCalculator.java
@@ -8,21 +8,37 @@ public static void main(String[] args){
 
 
     Scanner kbd = new Scanner(System.in);
-
+// takeInteger = kbd
     System.out.print("Please enter an initial deposit as a decimal: ");
     double c = kbd.nextDouble();
+
+
+    // c = entered number
 
     System.out.print("Enter a interest rate as a decimal: ");
     double r = kbd.nextDouble();
 
+
+
     System.out.print("Enter the amount of years as an integer: ");
     int t = kbd.nextInt();
+
+
+
 
     System.out.print("Enter a number of times to compound the interest per year as an integer: ");
     int n = kbd.nextInt();
 
 
+
+
     System.out.printf("Your investment is worth %.2f", Investment.investment(c, r, t, n));
+
+
+
+
+
+
 
 }
 
