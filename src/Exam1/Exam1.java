1abc8c7e0b1509175238eda9fc95a713dd0e44a9
treysyroka <treysyroka23@yahoo.com>
2022-09-27 15:50:40 -0400

    Exam 1 code

---
+++ src/Exam1/Exam1.java
@@ -0,0 +1,40 @@
+package Exam1;
+import java.util.Scanner;
+public class Exam1 {
+
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print(stringLength(getString(s)));
+
+    }
+public static int stringLength(int i){
+
+    int count = 0;
+    while (i > 0){
+        i = i / 10;
+        count = count + 1;
+
+    }
+    return count;
+
+
+}
+    public static int getString(Scanner s) {
+        while (true) {
+            System.out.println("Please enter an integer: ");
+            if (s.hasNextInt()) {
+                int i = s.nextInt();
+                if (i < 0)
+                    System.out.print("Error number should be positive, you entered: " + i + "\n");
+                else
+                    return i;
+            }
+            else {
+                String number = s.next();
+                System.out.print("Error: integer is expected. You put: " + number + "\n");
+            }
+
+        }
+    }
+}
