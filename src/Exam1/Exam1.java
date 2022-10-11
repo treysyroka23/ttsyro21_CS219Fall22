2f77e583e48fae1b0a1bd41d426632179664d11b
treysyroka <treysyroka23@yahoo.com>
2022-10-11 00:17:59 -0400

    updated exam

--- src/Exam1/Exam1.java
+++ src/Exam1/Exam1.java
@@ -25,11 +25,26 @@ public static int stringLength(int i){
             System.out.println("Please enter an integer: ");
             if (s.hasNextInt()) {
                 int i = s.nextInt();
+
                 if (i < 0)
                     System.out.print("Error number should be positive, you entered: " + i + "\n");
-                else
+                else if (i >= 7) {
+                    String fruit = Integer.toString(i);
+                    String index = fruit.substring(3, 7);
+                    i = Integer.parseInt(index);
                     return i;
+
+                }
+
+
+
+
+
             }
+
+
+
+
             else {
                 String number = s.next();
                 System.out.print("Error: integer is expected. You put: " + number + "\n");
